8620ac7067a7643ca37db412d5a30ec0b122b2ad
ephieo <60614102+ephieo@users.noreply.github.com>
2020-04-17 23:03:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ let slideIndex = 0;
 const slideShow () => {
 const slides = document.getElementByClassName("myslides");
   console.log(slides);
-
-
+for (let i =0; i < slides.length; i++){
+slides[i].style.disply="none";
+}
+slideIndex++ 
 
 };
+slideShow();
