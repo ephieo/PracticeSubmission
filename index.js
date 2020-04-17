873c2121b9ccce0ebe3788c6e1f20f6e9e623a33
ephieo <60614102+ephieo@users.noreply.github.com>
2020-04-17 23:10:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ for (let i =0; i < slides.length; i++){
 slides[i].style.disply="none";
 }
 slideIndex++ 
+  if(slideIndex > slides.length){
+  slideIndex = 1;
+  }
+  slides[slideIndex -1].style.display ="block";
 
 };
 slideShow();
