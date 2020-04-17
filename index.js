4f04ab03eae20435ce21693ba5d61c68784b5c5c
ephieo <60614102+ephieo@users.noreply.github.com>
2020-04-17 23:37:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let slideIndex = 0;
-const slideShow =() => {
-const slides = document.getElementByClassName("myslide");
-  //console.log(slides);
+const slideShow = () => {
+const slides = document.getElementsByClassName("myslide");
 for (let i =0; i < slides.length; i++){
 slides[i].style.display="none";
 }
@@ -11,6 +10,6 @@ slideIndex++
   }
   slides[slideIndex -1].style.display ="block";
 
-  setTimeout(slideShow, 3000);
+setTimeout(slideShow, 3000);
 };
 slideShow();
